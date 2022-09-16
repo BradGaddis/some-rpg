0eebbfcc5f330271848442365474c83dca401fc4
bgadd-brad <bradleygad@gmail.com>
2022-09-15 20:32:04 -0400

    starting skill editor window

---
+++ Assets/scripts/Skills/SkillObjNode.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Skill", menuName = "SkillObjNode", order = 0)]
+public class SkillObjNode : ScriptableObject {
+    [SerializeField]
+    string Description;
+    
+}
