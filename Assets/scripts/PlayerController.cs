e28955c865dbf6b21734cb63583cab976b64e56f
bgadd_brad <bradleygad@gmail.com>
2022-09-16 10:02:58 -0400

    adding simple background, player movement

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -1,20 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField]
+    float runSpeed;
+
+    Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // HandleMovement();   
+        HandleMovement(Input.GetAxisRaw("Horizontal"), 
+                       Input.GetAxisRaw("Vertical"));   
     }
 
-
+    private void HandleMovement(float moveHorizontal, float moveVertical)
+    {
+        rb.velocity = new Vector2(moveHorizontal, moveVertical).normalized * runSpeed;
+    }
 }
