f77219acbf7c0a495381b125e1eab25a8b01c487
bgadd-brad <bradleygad@gmail.com>
2022-09-15 20:36:49 -0400

    adding player and scripts

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Skill : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +13,8 @@ public class Skill : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // HandleMovement();   
     }
+
+
 }
