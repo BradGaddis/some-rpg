0cf343067e88d4876a02fa83c15682797df7dbf2
bgadd_brad <bradleygad@gmail.com>
2022-11-13 18:54:00 -0500

    add tools and player state machine

---
+++ Assets/scripts/Player/PlayerState.cs
@@ -0,0 +1,29 @@
+// Player state machine
+
+public enum PlayerState
+{
+    Idle,
+    Walking,
+    Running,
+    Jumping,
+    Falling,
+    Landing,
+    WallSliding,
+    WallJumping,
+    Dashing,
+    Attacking,
+    Stagger,
+    Interacting
+}
+
+// Player State Class
+[System.Serializable]
+public class PlayerStateMachine
+{
+    public PlayerStateMachine(PlayerState state)
+    {
+        this.state = state;
+    }
+
+    public PlayerState state { get; set; }
+}
