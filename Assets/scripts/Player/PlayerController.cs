f859d4a83508610a3a4933ed3b5fa7d7f02ef48f
bgadd_brad <bradleygad@gmail.com>
2022-11-13 18:54:29 -0500

    add tools and player state machine

--- Assets/scripts/Player/PlayerController.cs
+++ Assets/scripts/Player/PlayerController.cs
@@ -17,6 +17,13 @@ public class PlayerController : MonoBehaviour
     int speed;
     float jumpForce;
 
+    Tool hammer = new Tool();
+
+    // player state
+    PlayerState currentState = PlayerState.Idle;
+    // Player State Machine
+    PlayerStateMachine playerStateMachine = new PlayerStateMachine(PlayerState.Idle);
+
     Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
@@ -32,26 +39,83 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        HandleMovement(Input.GetAxisRaw("Horizontal"), 
-                       Input.GetAxisRaw("Vertical"));   
+        if (currentState != PlayerState.Interacting)
+        {
+            HandleMovement(Input.GetAxisRaw("Horizontal"), 
+                        Input.GetAxisRaw("Vertical"));   
+        } 
+        else
+        {   
+            HandleMovement(0, 0);
+        }
 
-        if (Input.GetKeyDown (KeyCode.Space)) {
+        if (Input.GetKeyDown (KeyCode.Space) && currentState != PlayerState.Jumping) {
             StartCoroutine (Jump());
         }   
+
+        HandleActions();
     }
 
     private void HandleMovement(float moveHorizontal, float moveVertical) => rb.velocity = new Vector2(moveHorizontal, moveVertical).normalized * runSpeed;
     
+    //  TODO fix double call
     IEnumerator Jump() {
+        // hold state
+        PlayerState prevState = currentState;
+        currentState = PlayerState.Jumping;
         Vector2 curscale = transform.localScale;
         float scaleChange = curscale.x + 0.5f;
         transform.localScale = new Vector2(scaleChange, scaleChange);
-        // animate the jump
-        return null;
+        yield return new WaitForSeconds(1f);
+        transform.localScale = new Vector3(1,1,1);
+        currentState = prevState;
     }
 
     public void AddPowerUp(PowerUp powerUp) {
         // add power up to player
         Debug.Log("Power Up Collected: " + powerUp.name);
+    }
+
+    // switch tool
+    IEnumerator UseTool(Tool tool = null) {
+        // hold state
+        PlayerState prevState = currentState;
+        currentState = PlayerState.Interacting;
+        Debug.Log("Using tool: " + tool.name);
+        yield return new WaitForSeconds(1f);
+        currentState = prevState;
+    }
+
+    // action handler
+    public void HandleActions() {
+        // lock movement
+        if (Input.GetKeyDown (KeyCode.E) && currentState != PlayerState.Jumping) {
+            StartCoroutine (UseTool(hammer));
+        }
+    }
+}
+
+// Tool class
+public class Tool {
+    // tool name
+    public string name;
+    // tool damage
+    int damage;
+    // tool speed
+    int speed;
+    // tool range
+    int range;
+    // tool cooldown
+    float cooldown;
+    // tool type
+    string type;
+
+    public Tool(string name = "Hammer", int damage = 1, int speed = 1, int range = 1, float cooldown = 1f, string type = "melee") {
+        this.name = name;
+        this.damage = damage;
+        this.speed = speed;
+        this.range = range;
+        this.cooldown = cooldown;
+        this.type = type;
     }
 }
