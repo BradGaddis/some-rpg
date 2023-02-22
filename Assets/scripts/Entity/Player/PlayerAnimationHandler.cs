ac7b188e8345b5cc439c94dd9a38dbc48a61cc23
bgadd_brad <bradleygad@gmail.com>
2023-02-22 17:00:26 -0500

    test player anim

--- Assets/scripts/Entity/Player/PlayerAnimationHandler.cs
+++ Assets/scripts/Entity/Player/PlayerAnimationHandler.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PlayerAnimationHandler : MonoBehaviour
 {
+
+    float timer = 0;
+    float timerDuration = 1;
+    private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (timer <= 0) {
+            bool isAttacking = animator.GetBool("isAttacking");
+            timer = timerDuration;
+            animator.SetBool("isAttacking", !isAttacking);
+        }
+        else {
+            timer -= Time.deltaTime;
+        }
     }
 }
